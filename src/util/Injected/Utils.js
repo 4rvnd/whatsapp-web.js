74b5185c9d43b3f41f9215271dc15f6fe5816772
Aravind <this.aravind@gmail.com>
2025-10-02 15:48:23 +0530

    fix: improve chat retrieval logic in getChat function

--- src/util/Injected/Utils.js
+++ src/util/Injected/Utils.js
@@ -526,7 +526,7 @@ exports.LoadUtils = () => {
     window.WWebJS.getChat = async (chatId, { getAsModel = true } = {}) => {
         const isChannel = /@\w*newsletter\b/.test(chatId);
         const chatWid = window.Store.WidFactory.createWid(chatId);
-        let chat;
+        let chat, chatLid;
 
         if (isChannel) {
             try {
@@ -539,7 +539,19 @@ exports.LoadUtils = () => {
                 chat = null;
             }
         } else {
-            chat = window.Store.Chat.get(chatWid) || (await window.Store.FindOrCreateChat.findOrCreateLatestChat(chatWid))?.chat;
+            chat = await window.Store.FindOrCreateChat.findOrCreateLatestChat(chatWid)
+            .then(chat => chat.chat)
+            .catch(async err => {
+                let query = window.require("WAWebContactSyncUtils").constructUsyncDeltaQuery([{
+                    type: "add",
+                    phoneNumber: chatWid.user
+                }]);
+                let result =  await query.execute();
+                if (result && Array.isArray(result.list) && result.list.length > 0 && result.list[0] && result.list[0].lid) {
+                    chatLid = window.Store.WidFactory.createWid(result?.list[0]?.lid)
+                    return await window.Store?.FindOrCreateChat?.findOrCreateLatestChat(chatLid)?.then(chat => chat.chat)?.catch(async err => {})
+                }
+            })
         }
 
         return getAsModel && chat
